ba03939260192c26371d6cde612664b50b97cb54
jmunoz <jmunoz@tuenti.com>
2017-12-22 17:42:02 +0100

    Added Counter and Navigation

---
+++ src/counter/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./counter.css";
+
+const Counter = ({ count }) => <span className="Counter">{count}</span>;
+
+export default Counter;
---
+++ src/navigation/index.js
@@ -0,0 +1,79 @@
+import React from "react";
+import "./navigation.css";
+
+export const NavigationMenu = ({ ariaLabel = "Navigation", children }) => (
+    <nav className="menu" aria-label={ariaLabel}>
+        {children}
+    </nav>
+);
+
+export const NavigationMenuItem = ({ children, href, current = false }) => (
+    <a
+        className={`menu-item ${current ? "selected" : ""}`}
+        href={href}
+        aria-current={`${current ? "page" : ""}`}
+    >
+        {children}
+    </a>
+);
+
+export const NavigationUnderline = ({
+    ariaLabel = "Navigation",
+    children,
+    alignRight = false
+}) => (
+    <nav
+        className={`UnderlineNav ${alignRight ? "UnderlineNav--right" : ""}`}
+        aria-label={ariaLabel}
+    >
+        <div className="UnderlineNav-body">{children}</div>
+    </nav>
+);
+
+export const NavigationUnderlineItem = ({
+    title,
+    children,
+    href,
+    current = false
+}) => (
+    <a
+        className={`UnderlineNav-item ${current ? "selected" : ""}`}
+        href={href}
+        role="tab"
+        title={title}
+    >
+        {children}
+    </a>
+);
+
+export const NavigationUnderlineActionGroup = ({ children }) => (
+    <div className="UnderlineNav-actions">{children}</div>
+);
+
+export const NavigationTabs = ({ children, ariaLabel = "Navigation" }) => (
+    <div className="tabnav">
+        <nav className="tabnav-tabs" aria-label={ariaLabel}>
+            {children}
+        </nav>
+    </div>
+);
+
+export const NavigationTabItem = ({ children, href, current = false }) => (
+    <a
+        href={href}
+        className="tabnav-tab selected"
+        aria-current={`${current ? "page" : ""}`}
+    >
+        {children}
+    </a>
+);
+
+export default {
+    Menu: NavigationMenu,
+    MenuItem: NavigationMenuItem,
+    Underline: NavigationUnderline,
+    UnderlineItem: NavigationUnderlineItem,
+    UnderlineActionGroup: NavigationUnderlineActionGroup,
+    Tabs: NavigationTabs,
+    TabItem: NavigationTabItem
+};
